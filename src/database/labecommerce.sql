89be4f4d6ef1bac1e8c9350f827c6ad55cdca0d6
mayaracgc <mayaracgcgmail.com>
2023-01-24 19:49:49 -0300

    atividade aprofundamento sql

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -13,7 +13,6 @@ VALUES ("a001", "João", "joao@gmail.com", "1234"),
         ("a002", "Maria", "maria@gmail.com", "5678"),
         ("a003", "José", "josé@gmail.com", "0910");
 
-SELECT * FROM users;
 
 CREATE TABLE products(
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
@@ -25,12 +24,83 @@ CREATE TABLE products(
 DROP TABLE products;
 
 INSERT INTO products (id, name, price, category)
-VALUES ("p001", "Café Geisha", 60.0, "Torra Média"), 
-        ("p002", "Café Acaiá", 80.0, "Torra Clara"), 
-        ("p003", "Café Kona", 95.0, "Torra Escura"),
-        ("p004", "Café Catuaí", 45.0, "Torra Escura"), 
-        ("p005", "Café Robusta", 50.0, "Torra Escura"), 
-        ("p006", "Café Arábica", 40.0, "Torra Clara"), 
-        ("p007", "Café Bourbon", 60.0, "Torra Média");
+VALUES ("p001", "Café Geisha", 60.00, "Torra Média"), 
+        ("p002", "Café Acaiá", 80.00, "Torra Clara"), 
+        ("p003", "Café Kona", 95.00, "Torra Escura"),
+        ("p004", "Café Catuaí", 45.00, "Torra Escura"), 
+        ("p005", "Café Robusta", 50.00, "Torra Escura"), 
+        ("p006", "Café Arábica", 40.00, "Torra Clara"), 
+        ("p007", "Café Bourbon", 60.00, "Torra Média");
 
+
+--retorna todos os usuários cadastrados
+SELECT * FROM users;
+
+--retorna todos os produtos cadastrados
 SELECT * FROM products;
+
+--retorna o resultado baseado no termo de busca
+SELECT * FROM products
+WHERE name = "Café Kona";
+
+--insere o item mockado na tabela users
+INSERT INTO users (id, name, email, password)
+VALUES ("a004", "Ana", "ana@gmail.com", "1112");
+
+--insere o item mockado na tabela products
+INSERT INTO products (id, name, price, category)
+VALUES ("p008", "Café Di Pássaro", 75.00, "Torra Média"),
+        ("p009", "Café Arara", 45.00, "Torra Clara"),
+        ("p010", "Café Clássico", 34.00, "Torra Média"),
+        ("p011", "Café Mantiqueira de Minas", 38.00, "Torra Média"),
+        ("p012", "Café Chapada de Minas", 45.00, "Torra Escura"),
+        ("p013", "Café Cerrado Mineiro", 55.00, "Torra Clara"),
+        ("p0014", "Kit 3 Cafés Moídos", 120.00, "Kit"),
+        ("p015", "Kit 4 Cafés Moídos", 140.00, "Kit"),
+        ("p016", "Kit 3 Cafés em Grãos", 150.00, "Kit"),
+        ("p017", "Kit 4 Cafés Moídos", 165.00, "Kit"),
+        ("p018", "Kit 30 Cápsulas de Café", 82.00, "Kit"),
+        ("p019", "Kit 40 Cápsulas de Café", 92.00, "Kit"),
+        ("p020", "Kit 50 Cápsulas de Café", 102.00, "Kit"),
+        ("p021", "Kit 3 Drips de Café", 115.00, "Kit"),
+        ("p022", "Kit 4 Drips de Café", 125.00, "Kit"),
+        ("p023", "Kit 3 Drips de Café + 1 Café Moído", 127.00, "Kit"),
+        ("p024", "Kit 4 Drips de Café + 1 Café Moído", 137.00, "Kit");
+
+--busca baseada no valor mockado
+SELECT * FROM products
+WHERE id = "p005";
+
+--delete a linha baseada no valor mockado
+DELETE FROM users 
+WHERE id = "a002";
+
+--delete a linha baseada no valor mockado
+DELETE FROM products
+WHERE id = "p004";
+
+--Edit User by id: edite a linha baseada nos valores mockados
+UPDATE users
+SET email = "joao@outlook.com", password = "1123"
+WHERE id = "a001";
+
+--Edit Product by id: edite a linha baseada nos valores mockados
+UPDATE products 
+SET price = 85.00 
+WHERE id = "p003";
+
+--retorna o resultado ordenado pela coluna email em ordem crescente
+SELECT * FROM users
+ORDER BY email ASC;
+
+--retorna o resultado ordenado pela coluna price em ordem crescente
+--limite o resultado em 20 iniciando pelo primeiro item
+SELECT * FROM products
+ORDER BY price ASC
+LIMIT 20;
+
+--mocke um intervalo de preços, por exemplo entre 100.00 e 300.00
+--retorna os produtos com preços dentro do intervalo mockado em ordem crescente
+SELECT * FROM products
+WHERE price >= 50 AND price <=100
+ORDER BY price ASC;
